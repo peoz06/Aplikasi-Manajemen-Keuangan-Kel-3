1aae2af1c1754ccba5f1277d9666897e75326c5f
peo <veojavaharjo@gmail.com>
2025-11-25 21:09:52 +0700

    Menambahkan PriorityItemActivty.kt

--- app/src/main/java/com/example/aplikasimanajemenkeuangankel3/PriorityItemActivity.kt
+++ app/src/main/java/com/example/aplikasimanajemenkeuangankel3/PriorityItemActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class PriorityItemActivty : AppCompatActivity() {
+class PriorityItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_priority_item_activty)
+        setContentView(R.layout.activity_priority_item)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
