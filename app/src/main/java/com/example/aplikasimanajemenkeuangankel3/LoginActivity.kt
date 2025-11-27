af10c9e049add483c106e7cd1156237951ba0345
ArifSyafar <arifsyafarian3@gmail.com>
2025-11-28 00:35:36 +0700

    Menambahkan halaman login di activity_login.xml dengan input text username dan password serta menambahkan sejumlah xml untuk di drawable seperti untuk backround card, bg untuk edit text, dan background gradient blue, beserta tombol login dan logika sederhana di LoginActivity.kt

---
+++ app/src/main/java/com/example/aplikasimanajemenkeuangankel3/LoginActivity.kt
@@ -0,0 +1,39 @@
+package com.example.aplikasimanajemenkeuangankel3
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val usernameInput = findViewById<EditText>(R.id.editTextTextUsername)
+        val passwordInput = findViewById<EditText>(R.id.editTextPassword)
+        val buttonLogin = findViewById<Button>(R.id.buttonLogin)
+
+        buttonLogin.setOnClickListener {
+
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Username dan Password tidak boleh kosong", Toast.LENGTH_SHORT).show()
+            } else {
+
+            }
+
+        }
+    }
+}
