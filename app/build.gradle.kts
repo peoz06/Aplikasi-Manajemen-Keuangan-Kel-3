c859212a1183ebd27d2ff6c1d72df8ed7e1b3793
ulilAb <ulilab65@gmail.com>
2025-12-01 11:09:51 +0700

    menambahkan isi fragment_profile.xml (kotak profil) dan layout barang dibawahnya

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.legacy.support.v4)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
